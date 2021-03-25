f8875e2561baeb012b187c507e9ca777f4214008
Joel Bradshaw <cincodenada@gmail.com>
2021-03-24 23:57:22 -0700

    Allow renaming accounts and setting up liabilities

--- ynab_to_ledger.rb
+++ ynab_to_ledger.rb
@@ -1,4 +1,24 @@
 require 'csv'
+require 'json'
+
+$config = JSON.parse(File.read('config.json'))
+
+def get_type(payee)
+  $config['account_types'].each do |type, accounts|
+    if accounts.include?(payee)
+      return type
+    end
+  end
+
+  return "Assets"
+end
+
+def translate_account(payee)
+  if $config['account_renames'].include?(payee)
+    payee = $config['account_renames'][payee]
+  end
+  return get_type(payee) + ':' + payee
+end
 
 def main
   entries = CSV.read(ARGV.first, headers: true, encoding: "BOM|UTF-8").map do |row|
@@ -18,19 +38,25 @@ def ledger_entry(row)
 
   month, day, year = row["Date"].split("/")
 
-  if row["Payee"].include?("Transfer :")
-    return if outflow == ""
-    source = row["Payee"].split(":").last.strip
+  if row["Category"] == "To be Budgeted"
+    source = "Income"
+  elsif row["Payee"] == "Starting Balance"
+    source = "Equity:Starting Balances"
+  elsif row["Payee"].include?("Transfer :")
+    account = row["Payee"].split(":").last.strip
+    source = translate_account(account)
   else
-    source = row["Category Group/Category"]
+    source = "Expenses:" + row["Category Group"] + ':' + row["Category"]
   end
 
   return if source == ""
 
+  suffix = row["Memo"] == "" ? "" : " ;" + row["Memo"]
+
   <<END
-#{year}/#{month}/#{day} #{row["Payee"]}#{row["Memo"]}
-    #{source}             #{outflow}
-    #{row["Account"]}     #{inflow}
+#{year}/#{month}/#{day} #{row["Payee"]}#{suffix}
+    #{source}  #{outflow}
+    #{translate_account(row["Account"])}  #{inflow}
 END
 end
 
