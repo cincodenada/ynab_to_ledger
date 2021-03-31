bd76cbc400ab9ed26f363b68627bb4b396b61772
Joel Bradshaw <cincodenada@gmail.com>
2021-03-30 23:37:05 -0700

    Split budget out into its own func

--- ynab_to_ledger.rb
+++ ynab_to_ledger.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'json'
+require 'date'
 
 $config = JSON.parse(File.read('config.json'))
 
@@ -22,7 +23,11 @@ end
 
 def main
   entries = CSV.read(ARGV.first, headers: true, encoding: "BOM|UTF-8").map do |row|
-    ledger_entry(row)
+    if row.has_key? "Budgeted"
+      budget_entry(row)
+    else
+      register_entry(row)
+    end
   end
 
   File.open("ynab_ledger.dat", "w") do |f|
@@ -30,29 +35,33 @@ def main
   end
 end
 
-def ledger_entry(row)
+def budget_entry(row)
+  date = Date.parse(row["Month"])
+  category = row["Category Group/Category"].sub(': ',':')
+
+  <<END
+#{date.strftime("%Y/%m/%d")} Budget
+    budget:#{category}  #{row["Budgeted"]}
+END
+end
+
+def register_entry(row)
   if row["Category"] == "To be Budgeted"
-    category = "Income"
+    source = "Income"
   elsif row["Payee"] == "Starting Balance"
-    category = "Equity:Starting Balances"
-  elsif row["Payee"].include?("Transfer :")
+    source = "Equity:Starting Balances"
+  elsif row["Payee"] and row["Payee"].include?("Transfer :")
     account = row["Payee"].split(":").last.strip
-    category = translate_account(account)
+    source = translate_account(account)
   else
-    category = "Expenses:" + row["Category Group"] + ':' + row["Category"]
+    source = "Expenses:" + row["Category Group"] + ':' + row["Category"]
   end
 
-  return if category == ""
+  return if source == ""
 
-  if row.has_key? "Budgeted"
-    inflow = row["Budgeted"]
-    dest = category
-  else
-    inflow = blank_if_zero(row["Inflow"])
-    outflow = blank_if_zero(row["Outflow"])
-    dest = translate_account(row["Account"])
-    source = category
-  end
+  inflow = blank_if_zero(row["Inflow"])
+  outflow = blank_if_zero(row["Outflow"])
+  dest = translate_account(row["Account"])
 
   return if inflow == "" && outflow == ""
 
